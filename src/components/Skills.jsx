fb1c11b15bd751198e6a339ba2b098add54f90e4
GussDev <91923480+gustavopumachagua@users.noreply.github.com>
2022-10-15 00:39:35 -0500

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -93,7 +93,7 @@ const Skills = () => {
           </p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-5 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-5 gap-16 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
